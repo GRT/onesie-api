2c8a3f4bcf59fa3012ae601a9f5c661d43458481
Mike Atkins <matkins@grtmail.com>
2016-11-19 15:21:29 -0500

    added test

---
+++ test/integration/status.spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const hapi = require('hapi');
+const onesie = require('../../lib');
+const expect = require('chai').expect;
+
+const wreck = require('wreck');
+
+describe('Status controller', function(done) {
+	beforeEach(function(next) {
+		this.server = new hapi.Server();
+		this.server.connection({ port: 3000 });
+		this.server.register(onesie);
+		this.server.start(next);
+
+	});
+
+	afterEach(function(done) {
+		this.server.stop(done);
+	});
+	it('should return ok, with status 200', function(done) {
+		wreck.get('http://localhost:3000/status', { json: true}, (error, response, payload) => {
+			expect(payload).to.have.property('status','ok');
+			expect(response.statusCode).to.equal(200);
+			done();
+		})
+	})
+});
