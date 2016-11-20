c79bb30f4b5ceb9f3a4c1e85c916012e6835dabe
Mike Atkins <matkins@grtmail.com>
2016-11-20 10:18:36 -0500

    whitespace fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ const eslint = require('gulp-eslint');
 
 gulp.task('eslint', () => {
   return gulp.src(['**/*.js','!node_modules/**'])
-	.pipe(eslint({configFile: './eslintrc.json'}))
-	.pipe(eslint.format())
-	.pipe(eslint.failAfterError());
+    .pipe(eslint({configFile: './eslintrc.json'}))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('default', ['eslint']);
